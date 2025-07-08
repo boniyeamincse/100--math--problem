79683c8286ee044a9c033da0b11676d963a09c2c
boniyeamincse <boniyeamin.cse1@gmail.com>
2025-07-09 00:02:39 +0600

    math Problem

---
+++ math_02.py
@@ -0,0 +1,13 @@
+# print all even number  from 1 to 100
+# even = []
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         even.append(i)
+# print(even)
+
+
+
+for num in range(1, 100):
+    if num % 2 == 0:
+        print(num)
+  
---
+++ math_05.py
@@ -0,0 +1,30 @@
+# # Print the sum of numbers from 1 to 100
+
+# n = 100
+# # By the formula
+# sum_number = n * (n + 1) // 2
+# print(f"Sum: {sum_number}")
+
+# Using for loop 
+#sum_number = 0
+#for i in range(1, 101):
+#    sum_number += i
+#print(f"Sum: {sum_number}")
+
+# using while loop 
+# i = 1
+# sum_number = 0
+# while i <= 100:
+#     sum_number += i
+#     i += 1
+# print("Sum:", sum_number)
+
+
+
+#using sum range
+
+# sum_number = sum(range(1, 101))
+# print(f"Sum :{sum_number}") 
+
+
+#using 
